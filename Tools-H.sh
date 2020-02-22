25d6749c4b3bd5aeb48c89f15e115747fe7a258d
M-hillah <60336165+M-hillah@users.noreply.github.com>
2020-02-23 05:29:09 +0700

    Add files via upload

---
+++ Tools-H.sh
@@ -0,0 +1,793 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+
+figlet -f standard "T O O L S-H" | lolcat
+
+echo "============================================" | lolcat
+echo      Author  :" M-Hillah" | lolcat
+echo      contack :" 0878xxxx" | lolcat
+echo "============================================" | lolcat
+date | lolcat
+echo "============================================" | lolcat
+echo $me"|"$ku" 00"$me" |"$i" [install bahan-bahan]"
+echo "============================================" | lolcat
+echo $me"|"$ku" 1"$me" |"$i" SocialFish"
+echo $me"|"$ku" 2"$me" |"$i" Shellphish"
+echo $me"|"$ku" 3"$me" |"$i" BlackEye"
+echo $me"|"$ku" 4"$me" |"$i" Red Hawk"
+echo $me"|"$ku" 5"$me" |"$i" Dtech"
+echo $me"|"$ku" 6"$me" |"$i" Termux lazy-Script"
+echo $me"|"$ku" 7"$me" |"$i" PUREBLOOD FRAMEWORK"
+echo $me"|"$ku" 8"$me" |"$i" Recondog TOOLS"
+echo $me"|"$ku" 9"$me" |"$i" Crips (Ip Tools)"
+echo $me"|"$ku" 10"$me"|"$i" Evil-URl"
+echo $me"|"$ku" 11"$me"|"$i" Lazymux Tools"
+echo $me"|"$ku" 12"$me"|"$i" Tools-X"
+echo $me"|"$ku" 13"$me"|"$i" ANGRY FUZZER"
+echo $me"|"$ku" 14"$me"|"$i" THECHOICE"
+echo $me"|"$ku" 15"$me"|"$i" USERECON"
+echo $me"|"$ku" 16"$me"|"$i" OWSCAN"
+echo $me"|"$ku" 17"$me"|"$i" CMS VUL-SCANNER"
+echo $me"|"$ku" 18"$me"|"$i" click jacking scanner"
+echo $me"|"$ku" 19"$me"|"$i" TM-scanner"
+echo $me"|"$ku" 20"$me"|"$i" Androbug framework"
+echo $me"|"$ku" 21"$me"|"$i" SQLISCAN"
+echo $me"|"$ku" 22"$me"|"$i" COMMIX"
+echo $me"|"$ku" 23"$me"|"$i" WPSEKu TOOL"
+echo $me"|"$ku" 24"$me"|"$i" ROUTERSPLOIT FRAMEWORK"
+echo $me"|"$ku" 25"$me"|"$i" NIKTO"
+echo $me"|"$ku" 26"$me"|"$i" CMSEEK SUIT"
+echo $me"|"$ku" 27"$me"|"$i" ZARP-LOCAL NETWORK TOOL"
+echo $me"|"$ku" 28"$me"|"$i" TM-VENOM"
+echo $me"|"$ku" 29"$me"|"$i" METASPLOIT FRAMEWORK"
+echo $me"|"$ku" 30"$me"|"$i" A-RAT"
+echo $me"|"$ku" 31"$me"|"$i" CHECK URL"
+echo $me"|"$ku" 32"$me"|"$i" DARKFLY TOOL"
+echo $me"|"$ku" 33"$me"|"$i" HASH CRACKER"
+echo $me"|"$ku" 34"$me"|"$i" WORDLIST GENERATE"
+echo $me"|"$ku" 35"$me"|"$i" SPEAK ENGINE"
+echo $me"|"$ku" 36"$me"|"$i" Gloom-Framework"
+echo $me"|"$ku" 37"$me"|"$i" BruteSpray"
+echo $me"|"$ku" 38"$me"|"$i" SUBSLITER"
+echo $me"|"$ku" 39"$me"|"$i" NetHunter In Termux"
+echo $me"|"$ku" 40"$me"|"$i" CREATE ZIP FILE"
+echo $me"|"$ku" 41"$me"|"$i" DARKSPLOIT"
+echo $me"|"$ku" 42"$me"|"$i" HUNNER FRAMEWORK"
+echo $me"|"$ku" 43"$me"|"$i" WEEVELY BACKDOOR-WEBSHELL"
+echo $me"|"$ku" 44"$me"|"$i" Cyber Scan"
+echo $me"|"$ku" 45"$me"|"$i" OPTIVA FRAMEWORK"
+echo $me"|"$ku" 46"$me"|"$i" BREACHER"
+echo $me"|"$ku" 47"$me"|"$i" STRIKER"
+echo $me"|"$ku" 48"$me"|"$i" HEARTBLEED SCANNER"
+echo $me"|"$ku" 49"$me"|"$i" SMBSCAN"
+echo $me"|"$ku" 50"$me"|"$i" LAZYFIGLET"
+echo $me"|"$ku" 51"$me"|"$i" HAKKU FRAMEWORK"
+echo $me"|"$ku" 52"$me"|"$i" WEBSPLOIT FRAMEWORK"
+echo $me"|"$ku" 53"$me"|"$i" TERMUX THEME"
+echo $me"|"$ku "54"$me"|"$i" AUTOPIXIEWPS"
+echo $me"|"$ku" 55"$me"|"$i" AIRCRACK-NG"
+echo $me"|"$ku" 56"$me"|"$i" GOBLIN WORD GENERATOR"
+echo $me"|"$ku" 57"$me"|"$i" WPSCAN"
+echo $me"|"$ku" 58"$me"|"$i" A2SV SSL-VUL-SCAN"
+echo $me"|"$ku" 59"$me"|"$i" VISQL [SQLI-VULN-SCANNERS]"
+echo $me"|"$ku" 99"$me"|"$i" EXIT"
+echo "============================================" | lolcat
+echo $me "┌==="$bi"["$ku"Tool-H"$i"]"$me"======="$me"["$ku""masukkan pilihan anda""$i"]"
+echo $me "¦"
+read -p " └── ~# " pil
+
+if [ $pil = 00 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg install root-repo
+pkg install unstable-repo
+pkg install X11-repo
+apt-get install figlet
+apt-get install ruby
+apt-get install perl
+apt-get intall bash
+gem install lolcat
+apt-get install git
+apt-get install wget
+apt-get install curl
+apt-get install python
+apt-get install nmap
+apt-get install tsu
+apt-get install python2
+apt-get install python3
+apt-get install php
+apt-get install sudo
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+chmod +x *
+bash blackeye.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x *
+php rhawk.php 
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+chmod +x *
+pip2 install requests
+python2 d-tect.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git
+cd Termux-Lazyscript
+chmod +x *
+sh setup.sh
+python2 ls.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/cr4shcod3/pureblood
+cd pureblood
+chmod +x *
+pip install -r requirements.txt
+python2 pureblood.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x *
+python2 dog.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Manisso/Crips
+cd Crips
+chmod +x *
+./install.sh
+python2 crips.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/EvilURL.git
+cd EvilURL
+chmod +x *
+ls
+python3 evilurl.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+fi
+
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+chmod +x *
+./install.aex
+Tool-X
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/ihebski/angryFuzzer
+cd angryFuzzer
+pip2 install -r requirements.txt
+pip2 install requests
+chmod +x *
+python2 angryFuzzer.py 
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/thechoice
+cd thechoice
+./thechoice
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/userrecon
+cd userrecon
+chmod +x *
+./userrecon.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/OWScan
+cd OWScan
+chmod +x *
+php owscan.php
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Dionach/CMSmap.git
+cd CMSmap
+chmod +x *
+python2 cmsmap.py 
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/D4Vinci/Clickjacking-Tester
+cd Clickjacking-Tester
+python2 Clickjacking_Tester.py file.txt
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/TM-scanner
+cd TM-scanner
+chmod +x *
+python2 tmscanner.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AndroBugs/AndroBugs_Framework
+cd AndroBugs_Framework
+chmod +x *
+python2 androbugs.py 
+fi
+
+if [ $pil = 21 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/sqliscan
+cd sqliscan
+chmod +x *
+./sqliscan.sh
+fi
+
+if [ $pil = 22 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/commixproject/commix
+cd commix
+chmod +x *
+python2 commix.py
+fi
+
+if [ $pil = 23 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/m4110k/WPSeku
+cd WPSeku
+chmod +x *
+pip install -r requirements.txt
+python wpseku.py
+fi
+
+if [ $pil = 24 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/reverse-shell/routersploit.git
+cd routersploit
+pip3 install -r requirments-dev.txt
+pip3 install -r requirments.txt
+pip3 install request
+python3 rsf.py
+fi
+
+if [ $pil = 25 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/sullo/nikto
+cd nikto
+chmod +x *
+perl nikto.pl
+fi
+
+if [ $pil = 26 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Tuhinshubhra/CMSeeK
+cd CMSeeK
+chmod +x *
+python cmseek.py 
+fi
+
+
+if [ $pil = 27 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/hatRiot/zarp
+cd zarp
+chmod +x zarp.py
+ python2 zarp.py
+fi
+
+if [ $pil = 28 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/tmvenom
+cd tmvenom
+chmod +x *
+python2 tmvenom.py
+fi
+
+if [ $pil = 29 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+wget https://Auxilus.github.io/metasploit.sh
+bash metasploit.sh
+./msfconsole
+fi
+
+if [ $pil = 30 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/xi4u7/A-Rat
+cd A-Rat
+chmod +x *
+python2 A-Rat.py
+fi
+
+if [ $pil = 31 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/checkURL
+cd checkURL
+chmod +x *
+python3 checkURL.py
+fi
+
+if [ $pil = 32 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+chmod +x *
+python2 install.py
+DarkFly
+fi
+
+if [ $pil = 33 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/ciku370/hasher
+cd hasher
+python2 hash.py
+fi
+
+if [ $pil = 34 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip install wordlist
+wordlist -h
+fi
+
+if [ $pil = 35 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/Termux-speak
+cd Termux-speak
+chmod +x *
+sh install.sh
+python2 t-speak.py
+fi
+
+if [ $pil = 36 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/StreetSec/Gloom-Framework.git
+cd Gloom-Framework
+chmod +x *
+python2 install.py
+python2 gloom.py
+pip2 install requests
+pip2 install bs4
+pip2 install beautifulsoup
+python2 gloom.py
+fi
+
+if [ $pil = 37 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/x90skysn3k/brutespray
+cd brutespray
+pip2 install -r requirements.txt
+python2 brutespray.py
+fi
+
+if [ $pil = 38 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+ git clone https://github.com/aboul3la/Sublist3r
+ cd Sublist3r
+ pip2 install requests
+ pip2 install requests
+ python2 sublist3r.py
+fi
+
+if [ $pil = 39 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd Nethunter-In-Termux
+chmod +x *
+./kalinethunter
+startkali
+fi
+
+if [ $pil = 40 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install zip
+apt install unzip
+zip name.zip 
+zip name.zip [your files]
+unzip proces : unzip name.zip
+fi
+
+if [ $pil = 41 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/LOoLzeC/DarkSploit
+cd install
+$ sh installtermux.sh
+pip2 install -r requirements.txt
+cd ..
+python2 DrXp.py
+fi
+
+if [ $pil = 42 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/b3-v3r/Hunner
+cd Hunner
+chmod 777 hunner.py
+python hunner.py
+fi
+
+if [ $pil = 43 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/glides/Weevely
+cd Weevely
+chmod +x *
+python2 weevely.py
+fi
+
+if [ $pil = 44 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/medbenali/CyberScan.git
+cd CyberScan
+python2 CyberScan.py -h
+fi
+
+if [ $pil = 45 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/joker25000/Optiva-Framework
+cd Optiva-Framework
+chmod +x *
+bash installer.sh
+python2 optiva.py
+fi
+
+if [ $pil = 46 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/s0md3v/Breacher
+cd Breacher
+python2 breacher.py
+fi
+
+if [ $pil = 47 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/s0md3v/Striker
+cd Striker
+pip2 install -r requirements.txt
+python2 striker.py
+fi
+
+if [ $pil = 48 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 
+git clone https://github.com/TechnicalMujeeb/HeartBleed.git 
+cd HeartBleed 
+chmod +x * 
+./install.sh 
+./hbleed
+fi
+
+if [ $pil = 49 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/smb-scanner
+cd smb-scanner
+cd modules
+chmod +x *
+cd . .
+chmod +x *
+ ./smbscan
+fi
+
+if [ $pil = 50 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/LazyFiglet
+cd LazyFiglet
+chmod +x *
+sh install.sh
+sh lfiglet.sh
+fi
+
+if [ $pil = 51 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4shadoww/hakkuframework
+cd hakkuframework
+chmod +x *
+python hakku.py
+fi
+
+if [ $pil = 52 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt-get install websploit
+websploit 
+fi
+
+if [ $pil = 53 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+./install.sh
+fi
+
+if [ $pil = 54 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/nxxxu/AutoPixieWps
+cd AutoPixie Wps
+chmod +x *
+python2 autopixie.py
+fi
+
+
+if [ $pil = 55 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt-get install aircrack-ng
+aircrack-ng
+fi
+
+if [ $pil = 56 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/GoblinWordGenerator
+cd GoblinWordGenerator
+chmod +x *
+python3 goblin.py
+fi
+
+if [ $pil = 57 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/wpscanteam/wpscan
+chmod 777 wpscan.rb 
+gem install bundle
+bundle install -j5
+ruby wpscan.rb
+fi
+
+if [ $pil = 58 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/hahwul/a2sv
+cd a2sv
+chmod +x *
+pip2 install -r requirements.txt
+python2 a2sv.py -h
+fi
+
+if [ $pil = 59 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/blackvkng/viSQL.git
+cd viSQL
+pip2 install -r requirements.txt
+python2 viSQL.py 
+fi
+
+if [ $pil = 99 ]
+then
+clear  
+figlet -f slant "W A I T"|lolcat                                                               
+sleep 1
+figlet -f slant "S H U T D O W N" | lolcat
+fi      
+echo $cy"Terima Kasih sudah mengunakan Tools ini dan semoga bermanfaat"
+                                                             
+
+
+
